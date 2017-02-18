41b7c6a984c8ce0b00e1536885238ff7d81f0e72
rdathis <athis.r@free.fr>
2017-02-18 17:04:10 +0100

    standard editor stuff.
    continue on testeditor.

--- TestEditorUtil/Program.cs
+++ TestEditorUtil/Program.cs
@@ -1,81 +1,111 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
 using System.Drawing.Design;
 using System.Windows.Forms;
 using System.Windows.Forms.Design;
-using System;
 
 class MyType
 {
-    private Foo foo = new Foo();
-    public Foo Foo { get { return foo; } }
+	//    private Bidule bidule = new Bidule();
+	//    public Bidule Bidule { get { return bidule; } }
+	
+	private List<Bidule> bidules= null;
+	public List<Bidule> Bidule  {
+		get { return bidules; }
+	}
+	
+	public MyType()	{
+		bidules=new List<Bidule>();
+		bidules.Add(new Bidule());
+		bidules.Add(new Bidule());
+	}
+	//}
 }
 
-[Editor(typeof(FooEditor), typeof(UITypeEditor))]
+[Editor(typeof(BiduleEditor), typeof(UITypeEditor))]
 [TypeConverter(typeof(ExpandableObjectConverter))]
-class Foo
+class Bidule
 {
-    private string bar;
-    public string Bar
-    {
-        get { return bar; }
-        set { bar = value; }
-    }
+	private string nom;
+	private string prenom;
+	private int age;
+	private DateTime entree;
+	public string Prenom {
+		get { return prenom; }
+		set { prenom=value; }
+	}
+	public string Nom     {
+		get { return nom; }
+		set { nom = value; }
+	}
+	public int Age     {
+		get { return age; }
+		set { age = value; }
+	}
+	public DateTime Entree {
+		get { return entree; }
+		set { entree = value; }
+	}
 }
-class FooEditor : UITypeEditor
+class BiduleEditor : UITypeEditor
 {
-    public override UITypeEditorEditStyle GetEditStyle(ITypeDescriptorContext context)
-    {
-        return UITypeEditorEditStyle.Modal;
-    }
-    public override object EditValue(ITypeDescriptorContext context, System.IServiceProvider provider, object value)
-    {
-        IWindowsFormsEditorService svc = provider.GetService(typeof(IWindowsFormsEditorService)) as IWindowsFormsEditorService;
-        Foo foo = value as Foo;
-        if (svc != null && foo != null)
-        {            
-            using (FooForm form = new FooForm())
-            {
-                form.Value = foo.Bar;
-                if (svc.ShowDialog(form) == DialogResult.OK)
-                {
-                    foo.Bar = form.Value; // update object
-                }
-            }
-        }
-        return value; // can also replace the wrapper object here
-    }
+	public override UITypeEditorEditStyle GetEditStyle(ITypeDescriptorContext context)
+	{
+		return UITypeEditorEditStyle.Modal;
+	}
+	public override object EditValue(ITypeDescriptorContext context, System.IServiceProvider provider, object value)
+	{
+		IWindowsFormsEditorService svc = provider.GetService(typeof(IWindowsFormsEditorService)) as IWindowsFormsEditorService;
+		
+		Bidule bidule = value as Bidule;
+		if (svc != null && bidule != null)
+		{
+			using (BiduleForm form = new BiduleForm())
+			{
+				form.Value = bidule.Nom;
+				if (svc.ShowDialog(form) == DialogResult.OK)
+				{
+					bidule.Nom = form.Value; // update object
+				}
+			}
+		}
+		
+		return value; // can also replace the wrapper object here
+	}
 }
-class FooForm : Form
+class BiduleForm : Form
 {
-    private TextBox textbox;
-    private Button okButton;
-    public FooForm() {
-        textbox = new TextBox();
-        Controls.Add(textbox);
-        okButton = new Button();
-        okButton.Text = "OK";
-        okButton.Dock = DockStyle.Bottom;
-        okButton.DialogResult = DialogResult.OK;
-        Controls.Add(okButton);
-    }
-    public string Value
-    {
-        get { return textbox.Text; }
-        set { textbox.Text = value; }
-    }
+	private TextBox textbox;
+	private Button okButton;
+	public BiduleForm() {
+		textbox = new TextBox();
+		Controls.Add(textbox);
+		okButton = new Button();
+		okButton.Text = "OK";
+		okButton.Dock = DockStyle.Bottom;
+		okButton.DialogResult = DialogResult.OK;
+		Controls.Add(okButton);
+	}
+	public string Value
+	{
+		get { return textbox.Text; }
+		set { textbox.Text = value; }
+	}
 }
 static class Program
 {
-    [STAThread]
-    static void Main()
-    {
-        Application.EnableVisualStyles();
-        Form form = new Form();
-        PropertyGrid grid = new PropertyGrid();
-        grid.Dock = DockStyle.Fill;
-        form.Controls.Add(grid);
-        grid.SelectedObject = new MyType();
-        Application.Run(form);
-    }
+	[STAThread]
+	static void Main()
+	{
+		Application.EnableVisualStyles();
+		Form form = new Form();
+		PropertyGrid grid = new PropertyGrid();
+		grid.Dock = DockStyle.Fill;
+		form.Controls.Add(grid);
+		
+		grid.SelectedObject = new MyType();
+		Application.Run(form);
+	}
 }
 
