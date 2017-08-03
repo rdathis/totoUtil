f69d10731eef5593f36f5c44058867185e5b59cf
rdathis <rdathis@cristallin.com>
2017-08-03 21:33:26 +0200

    stat moul begins

---
+++ cmdUtils/Objets/business/StatMoulReader.cs
@@ -0,0 +1,50 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: athis_000
+ * Date: 03/08/2017
+ * Time: 20:47
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.IO;
+
+namespace cmdUtils.Objets.business
+{
+	/// <summary>
+	/// Description of StatMoulReader.
+	/// </summary>
+	public class StatMoulReader
+	{
+		private static class  marquers {
+			public static readonly string
+			MARQUEUR_VISITE= "A",
+			MARQUEUR_STOCK="B",
+			MARQUEUR_STOCKNEG="C",
+			MARQUEUR_STOCKNUL="D",
+			MARQUEUR_STOCKPOS="E";
+		}
+		public const String defaultFileName="stat_moul.txt";
+		public StatMoulReader(String fileName=defaultFileName)
+		{
+		}
+		
+		private void readFile(String file) {
+			if (File.Exists(file)) {
+				String[] lignes = new MyUtil().readScript(file).Split('\n');
+				for (int numLigne = 1; numLigne < lignes.GetLength(0); numLigne++) {
+					String ligne = lignes[numLigne].Replace('\r', ' ').Trim();
+					String[] colonnes = ligne.Split('/');
+					if (colonnes.GetLength(0) > 1) {
+//			qty = tab[0];
+//			year = tab[1]
+
+						
+
+					}
+				}
+			}
+		}
+	}
+}
+
---
+++ cmdUtils/Objets/business/StatMoulRecap.cs
@@ -0,0 +1,57 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: athis_000
+ * Date: 03/08/2017
+ * Time: 20:53
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace cmdUtils.Objets.business
+{
+	/// <summary>
+	/// Description of StatMoulRecap.
+	/// </summary>
+	public class StatMoulRecap
+	{
+		private String year;
+		private int visiteQty;
+		private int stockQty;
+		private int stockNegQty;
+		private int stockZeroQty;
+		private int stockPosQty;
+		
+		public StatMoulRecap( //
+			String year, //
+			int visiteQty,  //
+			int stockQty,  //
+			int stockNegQty, int stockZeroQty, int stockPosQty) 
+		{
+			this.year=year;
+			this.visiteQty=visiteQty;
+			this.stockQty=stockQty;
+			this.stockNegQty=stockNegQty;
+			this.stockZeroQty=stockZeroQty;
+			this.stockPosQty=stockPosQty;
+		}
+		public String getYear() {
+			return year;
+		}
+		public int getVisiteQty() {
+			return visiteQty;
+		}
+		public int getStockQty() {
+			return stockQty;
+		}
+		public int getStockNegQty() {
+			return stockNegQty;
+		}
+		public int getStockZeroQty() {
+			return stockZeroQty;
+		}
+		public int getStockPosQty() {
+			return getStockPosQty;
+		}
+	}
+}
